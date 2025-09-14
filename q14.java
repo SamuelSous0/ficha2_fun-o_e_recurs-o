eba4246f26bcf8903e28e121a42218d7395cd36b
SamuelSous0 <samuel.00000859887@unicap.br>
2025-09-14 18:37:35 -0300

    Update q14.java

--- q14.java
+++ q14.java
@@ -1,8 +1,19 @@
 public class q14 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(ehPrimo(17,2));
     }
+
     public static boolean ehPrimo(int n, int divisor) {
-        
+        if (n <= 1) {
+            return false;
+        }
+        if (divisor == n) {
+            return true;
+        }
+        if (n % divisor == 0) {
+            return false;
+        }
+        return ehPrimo(n, divisor + 1);
     }
 }
